afa5804f4fdc54c23a776bed7c8117e05a8b389c
Fortino Garcia <fortino.garcia@colorado.edu>
2021-01-07 14:05:13 -0700

    Updated list of exported function calls.

--- src/Juqbox.jl
+++ src/Juqbox.jl
@@ -16,6 +16,16 @@ export bcparams, bcarrier2, gradbcarrier2
 export objparams, traceobjgrad, identify_guard_levels, identify_forbidden_levels, plotunitary, plotspecified, evalctrl
 export setup_ipopt_problem, Working_Arrays, estimate_Neumann!, assign_thresholds, setup_rotmatrices
 export run_optimizer, plot_conv_hist
+export setup_prior!, wmatsetup, assign_thresholds_old, assign_thresholds_freq 
+export init_adjoint!, tracefidabs2, tracefidreal,tracefidcomplex, trace_operator
+export adjoint_trace_operator!, penalf2a, penalf2aTrap, penalf2adj, penalf2adj!
+export penalf2grad, tikhonov_pen, tikhonov_grad!
+export KS!, accumulate_matrix!, controlfunc, controlfuncgrad!, rotmatrices!
+export fgradforce!, adjoint_grad_calc!, eval_forward, estimate_Neumann
+export calculate_timestep, KS_alloc, time_step_alloc, grad_alloc
+export eval_f_par, eval_g, eval_grad_f_par, eval_jac_g, intermediate_par
+export plot_forward, specify_level3, marginalize3
+export adjoint_tableau, step, step!, explicit_step, step_fwdGrad!, stepseparable, getgamma, magnus, neumann!
 
 # Julia versions prior to v"1.3.1" can't use LinearAlgebra's 5 argument mul!, routines
 # included here for backwards compatability
