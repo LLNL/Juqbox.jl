65b7a86829b716e03d237d5b9e943020697986da
Anders Petersson <petersson1@llnl.gov>
2023-09-04 10:03:38 -0700

    Call run_optimizer to do a derivative check with intermediate time levels

---
+++ examples/test-ipopt-interface.jl
@@ -0,0 +1,44 @@
+### Set up a test problem for the lifted approach with intermediate initial conditions
+# and continuity constraints
+using Juqbox
+using Printf
+using Plots
+
+include("two_sys_noguard.jl")
+
+# assign the target gate, sqrt(Swap12)
+Vtg = get_swap_1d_gate(2)
+target_gate = sqrt(Vtg)
+
+nTimeIntervals = 1 # 3 # 2 # 1
+
+retval = setup_std_model(Ne, Ng, f01, xi, xi12, couple_type, rot_freq, T, D1, target_gate, maxctrl_MHz=maxctrl_MHz, msb_order=msb_order, init_amp_frac=init_amp_frac, rand_seed=rand_seed, Pmin=Pmin, cw_prox_thres=cw_prox_thres, cw_amp_thres=cw_amp_thres, use_carrier_waves=use_carrier_waves, nTimeIntervals=nTimeIntervals)
+
+params = retval[1]
+pcof0 = retval[2]
+maxAmp = retval[3];
+
+params.traceInfidelityThreshold = 1e-3 # better than 99.9% fidelity
+
+Ntot = params.Ntot
+
+# Test non-zero Lagrange multipliers
+if params.nTimeIntervals > 1
+    for q = 1:params.nTimeIntervals-1
+        params.Lmult_r[q] = rand(Ntot, Ntot)
+        params.Lmult_i[q] = rand(Ntot, Ntot)
+    end
+else
+    # Only for testing the Lagrange multiplier term
+    params.Lmult_r = Vector{Matrix{Float64}}(undef, 1)
+    params.Lmult_i = Vector{Matrix{Float64}}(undef, 1)
+    params.Lmult_r[1] = rand(Ntot, Ntot)
+    params.Lmult_i[1] = rand(Ntot, Ntot)
+end
+
+println("Setup completed\n")
+
+println("Calling run_optimizer for derivative check")
+pcof = run_optimizer(params, pcof0, maxAmp, maxIter=0, derivative_test=true)
+
+println("IPOpt test completed")
