35e06f9ddcd26509e211afcf08d60c69830e21c6
Anders Petersson <petersson1@llnl.gov>
2023-12-14 16:32:46 -0800

    Testing the threading capability in Julia

--- examples/swap12-std-setup.jl
+++ examples/swap12-std-setup.jl
@@ -12,8 +12,8 @@ target_gate0 = get_swap_1d_gate(2)
 theta = pi/4
 target_gate = exp(-im*theta)*target_gate0
 
-maxIter = 200
-fidType = 1 # fidType = 1 for Frobenius norm^2, or fidType = 2 for Infidelity, or fidType = 3 for infid^2
+maxIter = 50
+fidType = 2 # fidType = 1 for Frobenius norm^2, or fidType = 2 for Infidelity, or fidType = 3 for infid^2
 
 retval = setup_std_model(Ne, Ng, f01, xi, xi12, couple_type, rot_freq, T, D1, target_gate, maxctrl_MHz=maxctrl_MHz, msb_order=msb_order, initctrl_MHz=initctrl_MHz, rand_seed=rand_seed, Pmin=Pmin, cw_prox_thres=cw_prox_thres, cw_amp_thres=cw_amp_thres, use_carrier_waves=use_carrier_waves, fidType=fidType)
 #true)
@@ -22,7 +22,7 @@ params = retval[1]
 pcof0 = retval[2]
 maxAmp = retval[3];
 
-params.traceInfidelityThreshold = 0.0 # 1e-3 # better than 99.9% fidelity
+params.traceInfidelityThreshold = 1e-5 # better than 99.9% fidelity
 
 println("Setup complete")
 
